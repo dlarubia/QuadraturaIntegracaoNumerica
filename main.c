0dc29e10dcd4668a8eea187ff5e7bbc4f35784d4
paulamacedo <paulamacedo@users.noreply.github.com>
2019-10-21 19:00:18 -0300

    Update main sequencial
    Acréscimo de cabeçalho do programa e leitura da entrada

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
+/*** Trabalho 1 - Versão Principal programa sequencial
+Autores: 
+Data: 15/10/2019
+Descrição: Implementação da versão sequencial do programa que utiliza o método de integração numérica retangular(ponto médio).
+ ***/
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
-int main () {
-	printf("Hello World!\n");
+
+int main (int c, char** argv){
+
+// O usuário deverá informar na linha de comando o intervalo [a,b] e o erro máximo e
+	if(argc!=4) {
+		puts("Entre com intervalo [a, b] e o erro na seguinte formatação : ./nome_programa a b e");
+		return 1;
+	}
+// Variáveis do programa
+	float intervaloA = atof(argv[1]);
+	float intervaloB = atof(argv[2]);
+	float erroMax = atof(argv[3]);
+	
 	return 0;
 }
