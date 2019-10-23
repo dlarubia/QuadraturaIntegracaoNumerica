2edbbeaf653a76624d1ea4ebe19e191b71cd2068
Daniel La Rubia <dlarubia@dcc.ufrj.br>
2019-10-23 19:49:26 -0300

    Nao possui mais as funçoes que devem ser calculadas

--- quadraturaAlgorithm.c
+++ quadraturaAlgorithm.c
@@ -16,51 +16,10 @@
  * 7- Encerrar o algoritmo e retornar a integral
  * 
  * --------
- * Funções que devem ser calculadas:
- * a(x) = 1 + x
- * b(x) = sqrt(1 - x²), -1 < x < 1
- * c(x) = sqrt(1 + x⁴)
- * d(x) = sen(x²)
- * f(x) = cos(e^-x)
- * g(x) = cos(e^-x) * x
- * h(x) = cos(e^-x) * (0.005 * x³ + 1)
- * 
- */
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <pthread.h>
-
-#define euler M_PI
-
-double a(double x) {
-    return 1 + x;
-}
-
-double b(double x) {
-    return sqrt(1 - pow(x,2));
-}
-
-double c(double x) {
-    return sqrt(1 + pow(x, 4));
-}
-
-double d(double x) {
-    return sin(pow(x,2));
-}
-
-double e(double x) {
-    return cos(pow(euler, -x));
-}
-
-double f(double x) {
-    return cos(pow(euler, -x)) * x;
-}
-
-double g(double x) {
-    return cos(pow(euler, -x)) * (0.005 * pow(x,3) + 1);
-}
 
 //TODO: Próximo passo é deixar de receber o parametro iteracoes e passar a controlar o término da função através do erro. Eliminar o for e tornar recursivo
 double integralPontoMedio(double (*f)(double), double a, double b, int iteracoes) {
