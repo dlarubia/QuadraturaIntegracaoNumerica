87a0b0b249b0ec364fea04306b223283062b9eed
Daniel La Rubia <dlarubia@dcc.ufrj.br>
2019-10-24 20:00:10 -0300

    Reordenando as funçoes para solucionar erros e primeira tentativa de ajuste na sincronizaçao de threads

--- quadraturaAlgorithm.c
+++ quadraturaAlgorithm.c
@@ -33,7 +33,7 @@ typedef struct Dados {
 Dados Buffer[N];
 double erroMaximo, valorIntegral = 0;
 int posicao;
-pthread_mutex_t mutex, mutex1, mutex2, mutex3, mutex4;
+pthread_mutex_t mutex, mutex1;
 
 Dados inicializaRetangulo(double a, double b, double (*f)(double)) {
     Dados retangulo;
@@ -77,43 +77,6 @@ double integralPontoMedioRecursivo(double (*f)(double), double a, double b, doub
     else return integralPontoMedio(f, a, (a + b)/2.0, erroMaximo) + integralPontoMedio(f, (b + a)/2.0, b, erroMaximo);
 }
 
-/*
-void *Integrar (void *tid) {
-
-    while(posicao != 0) {
-        //TODO: Verificar a corretude do controle de exclusão mútua
-        pthread_mutex_lock(&mutex);
-        calculaRetangulos(Buffer[posicao]);
-        pthread_mutex_unlock(&mutex);
-    }
-    
-    free(tid);
-    pthread_exit(NULL);
-}
-*/
-
-//TODO: Verificar a corretude do PUSH e POP na pilha, bem como o controle de sincronização
-void calculaRetangulos(Dados retangulo) {
-    //pop(1);
-    double pontoMedio = (retangulo.a + retangulo.b)/2.0;
-    double areaMaior = (retangulo.b - retangulo.a) * retangulo.f(pontoMedio);
-    double areaRetanguloEsquerda = ((pontoMedio - retangulo.a) * retangulo.f(pontoMedio));
-    double areaRetanguloDireita = ((retangulo.b - pontoMedio) * retangulo.f(pontoMedio));
-    double erro = areaMaior - (areaRetanguloEsquerda + areaRetanguloDireita);
-
-    if(abs(erro) > erroMaximo) {
-        pthread_mutex_lock(&mutex1);
-        Buffer[posicao + 1] = inicializaRetangulo(retangulo.a, pontoMedio, retangulo.f);
-        Buffer[posicao + 2] = inicializaRetangulo(pontoMedio, retangulo.b, retangulo.f);
-        pthread_mutex_unlock(&mutex1);
-    //    push(2);
-    }
-    else {
-        pthread_mutex_lock(&mutex2);
-        valorIntegral += areaMaior;
-        pthread_mutex_unlock(&mutex2);
-    }
-}
 
 void defineErroMaximo(double erro) {
     erroMaximo = erro;
@@ -121,14 +84,50 @@ void defineErroMaximo(double erro) {
 
 //TODO: Avaliar a necessidade do MUTEX
 void push(int n) {
-    pthread_mutex_lock(&mutex3);
     posicao++;
-    pthread_mutex_unlock(&mutex3);
 }
 
 //TODO: Avaliar a necessidade do MUTEX
 void pop(int n) {
-    pthread_mutex_lock(&mutex4);
     posicao--;
-    pthread_mutex_unlock(&mutex4);
+}
+
+
+//TODO: Verificar a corretude do PUSH e POP na pilha, bem como o controle de sincronização
+void calculaRetangulos(Dados retangulo) {
+    pop(1);
+    double pontoMedio = (retangulo.a + retangulo.b)/2.0;
+    double areaMaior = (retangulo.b - retangulo.a) * retangulo.f(pontoMedio);
+    double areaRetanguloEsquerda = ((pontoMedio - retangulo.a) * retangulo.f(pontoMedio));
+    double areaRetanguloDireita = ((retangulo.b - pontoMedio) * retangulo.f(pontoMedio));
+    double erro = areaMaior - (areaRetanguloEsquerda + areaRetanguloDireita);
+
+    pthread_mutex_unlock(&mutex);
+
+    if(abs(erro) > erroMaximo) {
+        pthread_mutex_lock(&mutex);
+        Buffer[posicao + 1] = inicializaRetangulo(retangulo.a, pontoMedio, retangulo.f);
+        Buffer[posicao + 2] = inicializaRetangulo(pontoMedio, retangulo.b, retangulo.f);
+        push(2);
+        pthread_mutex_unlock(&mutex);
+    }
+    else {
+        pthread_mutex_lock(&mutex1);
+        valorIntegral += areaMaior;
+        pthread_mutex_unlock(&mutex1);
+    }
+
+    pthread_mutex_lock(&mutex);
+}
+
+
+void *Integrar (void *tid) {
+
+    pthread_mutex_lock(&mutex);
+    while(posicao != 0) {
+        //TODO: Verificar a corretude do controle de exclusão mútua  
+        calculaRetangulos(Buffer[posicao]);
+    }
+    free(tid);
+    pthread_exit(NULL);
 }
