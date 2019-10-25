628b6400b5215ce44688602cd91b1f48a256b922
Daniel La Rubia <dlarubia@dcc.ufrj.br>
2019-10-24 21:43:46 -0300

    Prints para debugar modo ogro

--- quadraturaAlgorithm.c
+++ quadraturaAlgorithm.c
@@ -32,7 +32,7 @@ typedef struct Dados {
 
 Dados Buffer[N];
 double erroMaximo, valorIntegral = 0;
-int posicao;
+int posicao = -1;
 pthread_mutex_t mutex, mutex1;
 
 Dados inicializaRetangulo(double a, double b, double (*f)(double)) {
@@ -92,6 +92,17 @@ void pop(int n) {
     posicao--;
 }
 
+void preenchePilhaInicial(int nthreads, double a, double b, double (*f)(double)) {
+    printf("Vou preencher a pilha\n");
+    double espacamento = (b - a) / nthreads;
+
+    for (int i = 0; i < nthreads; i++) {
+        printf("Preenchendo a pilha %d!!\n", i);
+        Buffer[i] = inicializaRetangulo(a + i * espacamento, (a + i * espacamento) + espacamento, f);
+        push(1);
+    }
+}
+
 
 //TODO: Verificar a corretude do PUSH e POP na pilha, bem como o controle de sincronização
 void calculaRetangulos(Dados retangulo) {
@@ -102,16 +113,26 @@ void calculaRetangulos(Dados retangulo) {
     double areaRetanguloDireita = ((retangulo.b - pontoMedio) * retangulo.f(pontoMedio));
     double erro = areaMaior - (areaRetanguloEsquerda + areaRetanguloDireita);
 
+    printf("PontoMedio = %f\n", pontoMedio);
+    printf("areaMaior = %f\n", areaMaior);
+    printf("areaRetanguloEsquerda = %f\n", areaRetanguloEsquerda);
+    printf("areaRetanguloDireita = %f\n", areaRetanguloDireita);
+    printf("erro = %f\n", erro);
+
     pthread_mutex_unlock(&mutex);
 
     if(abs(erro) > erroMaximo) {
         pthread_mutex_lock(&mutex);
+        printf("Posicao = %d\n", posicao);
         Buffer[posicao + 1] = inicializaRetangulo(retangulo.a, pontoMedio, retangulo.f);
+        printf("Posicao + 1 = %d", posicao + 1);
         Buffer[posicao + 2] = inicializaRetangulo(pontoMedio, retangulo.b, retangulo.f);
+        printf("Posicao + 1 = %d", posicao + 2);
         push(2);
         pthread_mutex_unlock(&mutex);
     }
     else {
+        printf("Eu cheguei no else\n");
         pthread_mutex_lock(&mutex1);
         valorIntegral += areaMaior;
         pthread_mutex_unlock(&mutex1);
@@ -122,12 +143,19 @@ void calculaRetangulos(Dados retangulo) {
 
 
 void *Integrar (void *tid) {
-
+    int id = *(int*) tid;
+    printf("Eu to rodando!!! \n");
+    printf("posicao = %d\n", posicao);
+    printf("tid: %d\n", id);
     pthread_mutex_lock(&mutex);
-    while(posicao != 0) {
+    while(posicao != -1) {
+        printf("tid: %d\n", id);
         //TODO: Verificar a corretude do controle de exclusão mútua  
         calculaRetangulos(Buffer[posicao]);
     }
+    pthread_mutex_unlock(&mutex);
+    printf("A THREAD NÚMERO %d ACABOU COM CERTEZA!!!!\n", id);
+    posicao = -1;
     free(tid);
     pthread_exit(NULL);
 }
